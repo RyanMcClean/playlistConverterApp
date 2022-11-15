d0c6d9fcddd80f98ae0adf6a0e414fd63158d933
RyanU1328 <ryan1328@hotmail.com>
2022-11-15 16:29:28 +0000

    Debugging #5
    os.path.exist -> os.path.exists

--- musicMover.py
+++ musicMover.py
@@ -4,7 +4,7 @@ import time
 
 
 def musicCopy(musicLoc, musicMoveLoc):
-    if not os.path.exist("/mnt/windows-share/Users"):
+    if not os.path.exists(musicLoc):
         os.system("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq,pass=44Glenavna,ip=192.168.50.78")
     print("In background:\nChecking for laptop, then transferring music files")
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
