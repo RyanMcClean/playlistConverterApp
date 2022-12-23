f9707dc40965fdf789eabcf1911aebefda58de5e
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 16:34:39 +0000

    Debug 88

--- musicMover.py
+++ musicMover.py
@@ -11,12 +11,12 @@ def musicCopy(musicLoc, musicMoveLoc):
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
     if os.path.exists(musicLoc):
         print("Found laptop, checking for files to move now")
-        logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update "
+        logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update ",
                                "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/NAS/Music/"))
 
     if os.path.exists(musicRAIDLoc):
         print("\n\nFound RAID")
-        logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update "
+        logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update ",
                                 "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/RAID/PlexMedia/Music/"))
 
     if not os.path.exists(musicRAIDLoc):
