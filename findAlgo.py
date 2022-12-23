3e7975d86244eca4654d4ff5310e7329a11bf84f
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 13:15:24 +0000

    Debug 58
    Significant reformatting to findAlgo.py

--- findAlgo.py
+++ findAlgo.py
@@ -3,7 +3,7 @@ from time import sleep
 
 def m4aFinder(artist, album, name, pathToMusic):
     print("\n Searching for Artist: " + artist + " Album: " + album + " Song: " + name)
-    sleep(1)
+    sleep(2)
     # declare variables used in search function
     artistShort = artist
     artistCounter = 0
@@ -35,8 +35,9 @@ def m4aFinder(artist, album, name, pathToMusic):
         for num, i in enumerate(dirList):
             if i.startswith(artistShort):
                 del dirListCopy[num]
+                print("Found artist " + i)
+                sleep(2)
             while i.startswith(artistShort):
-                # print("Found artist " + i)
                 # similar as above with the artist, but searching through the albums now
                 if albumCounter > (0.95 * len(album)):
                     print("Album failure")
@@ -47,8 +48,10 @@ def m4aFinder(artist, album, name, pathToMusic):
                 y += 1
                 albumCounter += 1
                 for j in os.listdir(pathToMusic + i + "/"):
+                    if j.startswith(albumShort):
+                        print("Found album " + j)
+                        sleep(2)
                     while j.startswith(albumShort):
-                        # print("Found album " + j)
                         if nameCounter > (0.95 * len(name)):
                             print("Name failure")
                             break
@@ -69,56 +72,3 @@ def m4aFinder(artist, album, name, pathToMusic):
                                             stringToReturn = "/Music/" + i + "/" + j + "/" + k + "/" + l
                                             return stringToReturn
 
-    x = 0
-    y = 0
-    z = 0
-    while check == 0:
-        # Search for the artist name in the dir, if not found on first run through then
-        # delete a character from the end of the string and try again
-        if artistCounter > (0.95 * len(artist)):
-            # print("Artist failure")
-            check -= 1
-            break
-        if x > 0:
-            artistShort = artist[x:len(artist)]
-        x += 1
-        artistCounter += 1
-        # print("artistShort = " + artistShort)
-        for i in os.listdir(pathToMusic):
-            while i.endswith(artistShort):
-                # print("Found artist " + i)
-                # similar as above with the artist, but searching through the albums now
-                if albumCounter > (0.95 * len(album)):
-                    # print("Album failure")
-                    check -= 1
-                    break
-                if y > 0:
-                    albumShort = album[y:len(album)]
-                # print("albumShort = " + albumShort)
-                y += 1
-                albumCounter += 1
-                for j in os.listdir(pathToMusic + i + "/"):
-                    while j.startswith(albumShort):
-                        # print("Found album " + j)
-                        if nameCounter > (0.95 * len(name)):
-                            # print("Name failure")
-                            check -= 1
-                            break
-                        if z > 0:
-                            nameShort = name[z:len(name)]
-                        # print("nameShort = " + nameShort)
-                        z += 1
-                        nameCounter += 1
-                        for k in os.listdir(pathToMusic + i + "/" + j + "/"):
-                            if os.path.isfile(pathToMusic + i + "/" + j + "/" + k):
-                                if k.endswith(nameShort):
-                                    stringToReturn = "/Music/" + i + "/" + j + "/" + k
-                                    return stringToReturn
-                            else:
-                                for l in os.listdir(pathToMusic + i + "/" + j + "/" + k + "/"):
-                                    if os.path.isfile(pathToMusic + i + "/" + j + "/" + k + "/" + l):
-                                        if l.endswith(nameShort):
-                                            stringToReturn = "/Music/" + i + "/" + j + "/" + k + "/" + l
-                                            return stringToReturn
-
-    return None
