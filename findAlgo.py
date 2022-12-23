ed31f5fdbbb51fd9cfe6d6a50983839f8e6c118f
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 14:19:58 +0000

    Debug 69

--- findAlgo.py
+++ findAlgo.py
@@ -4,7 +4,7 @@ import logging
 def m4aFinder(artist, album, name, pathToMusic):
 
     logging.basicConfig(filename = "/export/NAS/playlistConverterLog.txt", filemode = "w",
-                        format = "%(name)s - %(levelname)s - %(message)s")
+                        format = "%(name)s - %(levelname)s - %(message)s", level = logging.INFO)
 
     logging.info("\n Searching for Artist: " + artist + " Album: " + album + " Song: " + name)
     # sleep(2)
