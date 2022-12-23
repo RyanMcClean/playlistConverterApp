2052d77e64a3dc9ceb4e2b9e0041d0e112c65cf7
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 16:39:57 +0000

    Debug 90

--- musicMover.py
+++ musicMover.py
@@ -6,18 +6,18 @@ import subprocess
 def musicCopy(musicLoc, musicMoveLoc):
     if not os.path.exists(musicLoc):
         logging.info(subprocess.Popen("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq, "
-                                      "pass=44Glenavna,ip=192.168.50.78", shell=True))
+                                      "pass=44Glenavna,ip=192.168.50.78", shell=True, stdout=subprocess.PIPE))
     print("In background:\nChecking for laptop, then transferring music files")
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
     if os.path.exists(musicLoc):
         print("Found laptop, checking for files to move now")
         logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update "
-                               "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/NAS/Music/", shell=True))
+                               "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/NAS/Music/", shell=True, stdout=subprocess.PIPE))
 
     if os.path.exists(musicRAIDLoc):
         print("\n\nFound RAID")
         logging.info(subprocess.Popen("sudo rsync -rpEogvht --delete --update "
-                                "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/RAID/PlexMedia/Music/", shell=True))
+                                "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/RAID/PlexMedia/Music/", shell=True, stdout=subprocess.PIPE))
 
     if not os.path.exists(musicRAIDLoc):
         print("No RAID")
