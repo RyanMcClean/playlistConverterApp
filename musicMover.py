e891816ce679d3e082ddf4b9a98d49aca65a6c0b
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 13:28:48 +0000

    Debug 62

--- musicMover.py
+++ musicMover.py
@@ -5,7 +5,8 @@ import time
 
 def musicCopy(musicLoc, musicMoveLoc):
     if not os.path.exists(musicLoc):
-        os.system("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq,pass=44Glenavna,ip=192.168.50.78")
+        os.system("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq,"
+                  "pass=44Glenavna,ip=192.168.50.78")
     print("In background:\nChecking for laptop, then transferring music files")
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
     if os.path.exists(musicLoc):
