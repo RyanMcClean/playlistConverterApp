e4d3b904f98e99bbf87da15c64e47060bd68e650
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 15:02:03 +0000

    Debug 71
    Added logging to music mover

--- musicMover.py
+++ musicMover.py
@@ -1,21 +1,22 @@
 import os
-import shutil
-import time
+import logging
 
 
 def musicCopy(musicLoc, musicMoveLoc):
+    logging.basicConfig(filename = "/export/NAS/fileMoverLog.txt", filemode = "w", level = logging.INFO)
     if not os.path.exists(musicLoc):
-        os.system("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq,"
-                  "pass=44Glenavna,ip=192.168.50.78")
+        logging.info(os.system("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ -o user=ryan_urq,"
+                  "pass=44Glenavna,ip=192.168.50.78"))
     print("In background:\nChecking for laptop, then transferring music files")
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
     if os.path.exists(musicLoc):
         print("Found laptop, checking for files to move now")
-        os.system("sudo rsync -rpEogvht --delete --update /mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/NAS/Music/")
+        logging.info(os.system("sudo rsync -rpEogvht --delete --update "
+                               "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/NAS/Music/"))
     if os.path.exists(musicRAIDLoc):
         print("\n\nFound RAID")
-        os.system("sudo rsync -rpEogvht --delete --update /mnt/windows-share/Users/ryan1/Music/Soggfy/ "
-                  "/export/RAID/PlexMedia/Music/")
+        logging.info(os.system("sudo rsync -rpEogvht --delete --update /mnt/windows-share/Users/ryan1/Music/Soggfy/ "
+                  "/export/RAID/PlexMedia/Music/"))
     if not os.path.exists(musicRAIDLoc):
         print("No RAID")
     print("Music dir up-to-date")
