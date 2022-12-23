cd170bc2e3866aaa96aacc8c721237f641c28376
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 17:02:57 +0000

    Debug 97

--- musicMover.py
+++ musicMover.py
@@ -6,27 +6,27 @@ import subprocess
 def musicCopy(musicLoc, musicMoveLoc):
     if not os.path.exists(musicLoc):
         logging.info("Mounting laptop")
-        output = subprocess.Popen("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ "
+        output, err = subprocess.Popen("sudo mount.cifs //ryan_urq_laptop/c/ /mnt/windows-share/ "
                                       "-o user=ryan_urq, pass=44Glenavna,ip=192.168.50.78",
                                       shell=True, stdout=subprocess.PIPE)
-        logging.info(output.communicate()[0])
+        logging.info(output.communicate())
     print("In background:\nChecking for laptop, then transferring music files")
     musicRAIDLoc = "/export/RAID/PlexMedia/Music/"
     if os.path.exists(musicLoc):
         print("Found laptop, checking for files to move now")
         logging.info("Moving to NAS")
-        output = subprocess.Popen("sudo rsync -rpEogvht --delete --update "
+        output, err = subprocess.Popen("sudo rsync -rpEogvht --delete --update "
                                   "/mnt/windows-share/Users/ryan1/Music/Soggfy/ "
                                   "/export/NAS/Music/", shell=True, stdout=subprocess.PIPE)
-        logging.info(output.communicate()[0])
+        logging.info(output.communicate())
 
     if os.path.exists(musicRAIDLoc):
         print("\n\nFound RAID")
         logging.info("Moving to RAID")
-        output = subprocess.Popen("sudo rsync -rpEogvht --delete --update "
+        output, err = subprocess.Popen("sudo rsync -rpEogvht --delete --update "
                                       "/mnt/windows-share/Users/ryan1/Music/Soggfy/ /export/RAID/PlexMedia/Music/",
                                       shell=True, stdout=subprocess.PIPE)
-        logging.info(output.communicate()[0])
+        logging.info(output.communicate())
 
     if not os.path.exists(musicRAIDLoc):
         print("No RAID")
