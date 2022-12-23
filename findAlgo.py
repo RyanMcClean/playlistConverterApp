ee0a076759a54cd915a6497adbb4088bbd0e218e
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-23 14:00:37 +0000

    Debug 65

--- findAlgo.py
+++ findAlgo.py
@@ -1,8 +1,11 @@
 import os
-from time import sleep
+import logging
 
 def m4aFinder(artist, album, name, pathToMusic):
-    print("\n Searching for Artist: " + artist + " Album: " + album + " Song: " + name)
+
+    logging.basicConfig(filename = "/export/NAS/playlistConverterLog.txt")
+
+    logging.info("\n Searching for Artist: " + artist + " Album: " + album + " Song: " + name)
     # sleep(2)
     # declare variables used in search function
     artistShort = artist
@@ -25,52 +28,52 @@ def m4aFinder(artist, album, name, pathToMusic):
         # Search for the artist name in the dir, if not found on first run through then
         # delete a character from the end of the string and try again
         if artistCounter > (0.95 * len(artist)):
-            print("Artist failure, cancelling search\n\n")
+            logging.info("Artist failure, cancelling search\n\n")
             return None
         if x > 0:
             artistShort = artist[:-x]
         x += 1
         artistCounter += 1
-        # print("artistShort = " + artistShort)
+        # logging.info("artistShort = " + artistShort)
         for num, i in enumerate(dirList):
             if i.startswith(artistShort):
                 del dirListCopy[num]
-                print("Found artist " + i)
+                logging.info("Found artist " + i)
                 # sleep(2)
             while i.startswith(artistShort):
                 # similar as above with the artist, but searching through the albums now
                 if albumCounter > (0.95 * len(album)):
-                    print("Album failure")
+                    logging.info("Album failure")
                     break
                 if y > 0:
                     albumShort = album[:- y]
-                # print("albumShort = " + albumShort)
+                # logging.info("albumShort = " + albumShort)
                 y += 1
                 albumCounter += 1
                 for j in os.listdir(pathToMusic + i + "/"):
                     if j.startswith(albumShort):
-                        print("Found album " + j)
+                        logging.info("Found album " + j)
                         # sleep(2)
                     while j.startswith(albumShort):
                         if nameCounter > (0.95 * len(name)):
-                            print("Name failure")
+                            logging.info("Name failure")
                             break
                         if z > 0:
                             nameShort = name[z:len(name)]
-                        # print("nameShort = " + nameShort)
+                        # logging.info("nameShort = " + nameShort)
                         z += 1
                         nameCounter += 1
                         for k in os.listdir(pathToMusic + i + "/" + j + "/"):
                             if os.path.isfile(pathToMusic + i + "/" + j + "/" + k):
                                 if k.endswith(nameShort):
-                                    print("Found song file")
+                                    logging.info("Found song file")
                                     stringToReturn = "/Music/" + i + "/" + j + "/" + k
                                     return stringToReturn
                             else:
                                 for l in os.listdir(pathToMusic + i + "/" + j + "/" + k + "/"):
                                     if os.path.isfile(pathToMusic + i + "/" + j + "/" + k + "/" + l):
                                         if l.endswith(nameShort):
-                                            print("Found song file")
+                                            logging.info("Found song file")
                                             stringToReturn = "/Music/" + i + "/" + j + "/" + k + "/" + l
                                             return stringToReturn
 
