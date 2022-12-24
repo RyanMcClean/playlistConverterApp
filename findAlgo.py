afddb7dba74ad5d4e57510a7dba4f3546c16611a
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-24 19:18:29 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -16,6 +16,9 @@ def m4aFinder(artist, album, name, pathToMusic):
         if (i[0] != artist[0]):
             del dirList[num]
             print("removed " + i )
+        elif (i[0:22] != artist[0:2]):
+            del dirList[num]
+            print("removed " + i )
 
     print(len(dirList))
     sleep(10)
