f14f5463ebaf0ed53c945220ce0faa287b217d2d
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-24 23:12:28 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -27,6 +27,7 @@ def m4aFinder(artist, album, name, pathToMusic):
             logging.info("Searching for " + artistToFind[0:x])
             if (i.startswith(artistToFind[0:x])):
                 logging.info("Found artist " + i + "\n")
+
                 albumDirs = os.listdir(pathToMusic + "/" + i)
                 y = len(album)
                 for j in albumDirs:
@@ -38,11 +39,38 @@ def m4aFinder(artist, album, name, pathToMusic):
                         logging.info("Searching for " + album[0:y])
                         if (j.lower().startswith(album[0:y].lower())):
                             logging.info("Found album " + j + "\n\n")
-                            return ("Found " + i + "/" + j)
+
+                            songDir = os.listdir(pathToMusic + "/" + i + "/" + j)
+                            z = len(name)
+                            for k in songDir:
+
+                                while True:
+
+                                    logging.info("Searching in " + k)
+                                    logging.info("Searching for " + name[0:z])
+                                    if os.path.isfile(pathToMusic + "/" + i + "/" + j + "/" + k):
+                                        if (k.startswith(name[0:z])):
+                                            logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k)
+                                            return (pathToMusic + "/" + i + "/" + j + "/" + k)
+                                    else:
+                                        cdDir = os.listdir(pathToMusic + "/" + i + "/" + j + "/" + k)
+                                        for l in cdDir:
+
+                                            while True:
+
+                                                logging.info("Searching in " + l)
+                                                logging.info("Searching for " + l)
+                                                if l.startswith(name[0:z]):
+                                                    logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
+                                                    return (pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
+
+                                        z -= 1
+                                        if z < (0.5 * len(name)):
+                                            break
+
                         y -= 1
                         if y < (0.15 * len(album)):
                             break
-
 
             x -= 1
             if x < (0.5 * len(artistToFind)) :
