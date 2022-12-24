31715ef9c2d8746edac7f92198ff98ae6d144419
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-24 23:24:04 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -37,36 +37,37 @@ def m4aFinder(artist, album, name, pathToMusic):
                         #     break
                         logging.info("Searching in " + j)
                         logging.info("Searching for " + album[0:y])
-                        if (j.lower().startswith(album[0:y].lower())):
-                            logging.info("Found album " + j + "\n\n")
+                        if y > 0:
+                            if (j.lower().startswith(album[0:y].lower())):
+                                logging.info("Found album " + j + "\n\n")
 
-                            songDir = os.listdir(pathToMusic + "/" + i + "/" + j)
-                            z = 0
-                            for k in songDir:
+                                songDir = os.listdir(pathToMusic + "/" + i + "/" + j)
+                                z = 0
+                                for k in songDir:
 
-                                while True:
+                                    while True:
 
-                                    logging.info("Searching in " + k)
-                                    logging.info("Searching for " + name[z:len(name)])
-                                    if os.path.isfile(pathToMusic + "/" + i + "/" + j + "/" + k):
-                                        if (k.endswith(name[0:z])):
-                                            logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k)
-                                            return (pathToMusic + "/" + i + "/" + j + "/" + k)
-                                    else:
-                                        cdDir = os.listdir(pathToMusic + "/" + i + "/" + j + "/" + k)
-                                        for l in cdDir:
+                                        logging.info("Searching in " + k)
+                                        logging.info("Searching for " + name[z:len(name)])
+                                        if os.path.isfile(pathToMusic + "/" + i + "/" + j + "/" + k):
+                                            if (k.endswith(name[0:z])):
+                                                logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k)
+                                                return (pathToMusic + "/" + i + "/" + j + "/" + k)
+                                        else:
+                                            cdDir = os.listdir(pathToMusic + "/" + i + "/" + j + "/" + k)
+                                            for l in cdDir:
 
-                                            while True:
+                                                while True:
 
-                                                logging.info("Searching in " + l)
-                                                logging.info("Searching for " + l)
-                                                if l.startswith(name[z:len(name)]):
-                                                    logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
-                                                    return (pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
+                                                    logging.info("Searching in " + l)
+                                                    logging.info("Searching for " + l)
+                                                    if l.startswith(name[z:len(name)]):
+                                                        logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
+                                                        return (pathToMusic + "/" + i + "/" + j + "/" + k + "/" + l)
 
-                                        z += 1
-                                        if z > (0.5 * len(name)):
-                                            break
+                                            z += 1
+                                            if z > (0.5 * len(name)):
+                                                break
 
                         y -= 1
                         if y < (0.15 * len(album)) or y < 2:
