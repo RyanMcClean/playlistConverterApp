cf1719437b770092f339f3b7ea8b77f3157d4f32
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-24 20:38:34 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -6,6 +6,9 @@ def m4aFinder(artist, album, name, pathToMusic):
 
     logging.info("Searching for Artist: " + artist + " Album: " + album + " Song: " + name + "\n")
 
+    artist = artist.split(", ")
+    if artist is list:
+        artist = artist[0]
     dirList = os.listdir(pathToMusic)
     dirList.sort()
     x = len(artist)
