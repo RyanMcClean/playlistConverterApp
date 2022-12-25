aeb66580e78914dee6a82c967f09232b63f3aff4
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-25 00:35:27 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -50,8 +50,7 @@ def m4aFinder(artist, album, name, pathToMusic):
                                         logging.info("Searching in " + k)
                                         logging.info("Searching for " + name[z:len(name)])
                                         if os.path.isfile(pathToMusic + "/" + i + "/" + j + "/" + k):
-                                            if not k.lower().startswith(name[0:1].lower()):
-                                                break
+
                                             elif (k.lower().endswith(name[0:z].lower())):
                                                 logging.info("Found song " + pathToMusic + "/" + i + "/" + j + "/" + k + "\n\n")
                                                 return (pathToMusic + "/" + i + "/" + j + "/" + k)
@@ -60,8 +59,7 @@ def m4aFinder(artist, album, name, pathToMusic):
                                             for l in cdDir:
                                                 a = 0
                                                 while True:
-                                                    if not l.startswith(name[0:1]):
-                                                        break
+
 
                                                     logging.info("Searching in " +  k + "/" + l)
                                                     logging.info("Searching for " + l)
