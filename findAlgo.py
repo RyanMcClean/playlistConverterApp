8e46cc7a3f1da356a9252870ed84e0c7d9f85add
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-25 21:26:58 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -7,7 +7,8 @@ def m4aFinder(artist, album, name, pathToMusic):
     logging.info("Searching for Artist: " + artist + " Album: " + album + " Song: " + name + "\n")
 
     for dirs in os.scandir(pathToMusic):
-        print(dirs)
+        if dirs.is_dir():
+            print(dirs.path)
 
     logging.info("\n\n\n None found \n\n\n")
     return None
