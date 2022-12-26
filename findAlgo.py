3a40141c376448978241ecbc6c20cc232ae11460
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-26 08:39:41 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -1,23 +1,15 @@
 import os
 import logging
 from time import sleep
+from pathlib import Path
 
 def m4aFinder(artist, album, name, pathToMusic):
 
     logging.info("Searching for Artist: " + artist + " Album: " + album + " Song: " + name + "\n")
 
-    musicPaths = os.scandir(pathToMusic)
-
-    print(len(musicPaths))
-    sleep(5)
-
-    for dirs in musicPaths:
-        if dirs.is_dir():
-            print(dirs.path)
-            if dirs.startswith(artist[0:1]):
-                musicPaths.remover(dirs)
-
-    print(len(musicPaths))
+    for path in Path(pathToMusic).iterdir():
+        if path.is_dir():
+            print(path)
 
     logging.info("\n\n\n None found \n\n\n")
     return None
