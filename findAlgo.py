eddd878f5edf955ca313808f1e02080e7c63e265
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-26 14:07:16 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -9,19 +9,15 @@ def m4aFinder(artist, album, name, pathToMusic):
     artistDirs = os.listdir(pathToMusic)
 
     artist = artist.replace(" ", "")
-    print(len(artistDirs))
     sleep(2.5)
 
-    for num, i in artistDirs:
+    for num, i in enumerate(artistDirs):
         i = i.replace(" ", "")
         if not i.startswith(artist[0:1]):
             continue
         elif i.startswith(artist[0:2]):
             return i
 
-
-
-    print((artistDirs))
 
     logging.info("\n\n\n None found \n\n\n")
     return None
