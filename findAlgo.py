2bcf5265d04ed960504389da2c383535984bc43e
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-27 03:49:07 +0000

    Debug 177

--- findAlgo.py
+++ findAlgo.py
@@ -32,6 +32,7 @@ def m4aFinder(artist, album, name, pathToMusic, v):
         artistCheck = artistCheck.replace("？", "?")
         artistCheck = artistCheck.replace("Æ", "Æ")
         artistCheck = artistCheck.replace("＂", "\"")
+        artistCheck = artistCheck.replace("￤", "|")
 
         if artistCheck.lower().startswith(artist.lower()):
             if v == "y":
@@ -47,6 +48,7 @@ def m4aFinder(artist, album, name, pathToMusic, v):
                 albumCheck = albumCheck.replace("．", ".")
                 albumCheck = albumCheck.replace("？", "?")
                 albumCheck = albumCheck.replace("＂", "\"")
+                albumCheck = albumCheck.replace("￤", "|")
 
                 if not albumCheck.lower().startswith(album[0:1].lower()):
                     if v =="y":
@@ -72,6 +74,7 @@ def m4aFinder(artist, album, name, pathToMusic, v):
                                 songCheck = songCheck.replace("．", ".")
                                 songCheck = songCheck.replace("？", "?")
                                 songCheck = songCheck.replace("＂", "\"")
+                                songCheck = songCheck.replace("￤", "|")
 
                                 if not songCheck.lower().endswith(name[len(name)-1:len(name)].lower()):
                                     if v == "y":
@@ -100,6 +103,7 @@ def m4aFinder(artist, album, name, pathToMusic, v):
                                     songCheck = songCheck.replace("．", ".")
                                     songCheck = songCheck.replace("？", "?")
                                     songCheck = songCheck.replace("＂", "\"")
+                                    songCheck = songCheck.replace("￤", "|")
 
                                     if not songCheck.lower().endswith(name[len(name)-1:len(name)].lower()):
                                         if v == "y":
