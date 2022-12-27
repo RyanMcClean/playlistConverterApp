553e4e573177be07ea900e0b8689a3f0c19cb7dc
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-27 01:55:33 +0000

    Debug 177

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     filesMissingPerPlaylist.sort()
 
 
-    if filesMissingPerPlaylist is list:
+    if isinstance(filesMissingPerPlaylist, list):
 
         filesMissingPerPlaylist.sort()
         for i in filesMissingPerPlaylist:
