c557735840bd06236082f87d8442ff4a27ac3ff1
RyanUrq <rurquhart01@qub.ac.uk>
2022-12-27 00:39:20 +0000

    Debug 177

--- main.py
+++ main.py
@@ -18,57 +18,57 @@ playlist = []
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename = "/export/NAS/playlistConverterLog.txt", filemode = "w",
-                        format = "%(name)s - %(levelname)s - %(message)s", level = logging.INFO)
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    v = input("Verbose? y/n\n\n")
-
-    # move music from laptop to NAS
-    x = threading.Thread(target=musicCopy, args=(pathToOriginalMusic, pathToMusic))
-    x.start()
+    # logging.basicConfig(filename = "/export/NAS/playlistConverterLog.txt", filemode = "w",
+    #                     format = "%(name)s - %(levelname)s - %(message)s", level = logging.INFO)
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    #
+    # v = input("Verbose? y/n\n\n")
+    #
+    # # move music from laptop to NAS
+    # x = threading.Thread(target=musicCopy, args=(pathToOriginalMusic, pathToMusic))
+    # x.start()
 
     # Download zipped file of spotify playlists
     y = threading.Thread(target=playlistdownloader, args=(downloadsPath,))
     y.start()
 
-    # unzip playlists file
-    unzipper(downloadsPath, downloadsPath + namePlaylistsDir)
-
-
-
-
-    fileList = selection(downloadsPath + namePlaylistsDir)
-    counter = 0
-    loopCounter = 0
-    filesMissingPerPlaylist = []
-
-    if isinstance(fileList, str):
-        playlist = CSV_Extraction(downloadsPath + namePlaylistsDir, pathToMusic, fileList, v)
-        counter += playlistFileCreation(pathToFinalPlaylist, playlist)
-        filesMissingPerPlaylist = (fileList + "\t-\t" + str(counter))
-
-    else:
-        for i in fileList:
-            loopCounter += 1
-            print("%2d" % ((loopCounter/len(os.listdir(downloadsPath + namePlaylistsDir)))*100) + "%")
-            print("Converting " + i)
-            playlist = CSV_Extraction(downloadsPath + namePlaylistsDir, pathToMusic, i, v)
-            counterAdder = playlistFileCreation(pathToFinalPlaylist, playlist)
-            if counterAdder > 0:
-                filesMissingPerPlaylist.append((str(counterAdder) + "\tsongs missing from:\t" + i))
-            counter += counterAdder
-            os.system('cls' if os.name == 'nt' else 'clear')
-
-    print(str(counter) + " total missing songs from library")
-
-    filesMissingPerPlaylist.sort()
-
-    try:
-        for i in filesMissingPerPlaylist:
-            print(i)
-            logging.info(i)
-
-    except:
-        print(filesMissingPerPlaylist)
-        logging.info(filesMissingPerPlaylist)
+    # # unzip playlists file
+    # unzipper(downloadsPath, downloadsPath + namePlaylistsDir)
+    #
+    #
+    #
+    #
+    # fileList = selection(downloadsPath + namePlaylistsDir)
+    # counter = 0
+    # loopCounter = 0
+    # filesMissingPerPlaylist = []
+    #
+    # if isinstance(fileList, str):
+    #     playlist = CSV_Extraction(downloadsPath + namePlaylistsDir, pathToMusic, fileList, v)
+    #     counter += playlistFileCreation(pathToFinalPlaylist, playlist)
+    #     filesMissingPerPlaylist = (fileList + "\t-\t" + str(counter))
+    #
+    # else:
+    #     for i in fileList:
+    #         loopCounter += 1
+    #         print("%2d" % ((loopCounter/len(os.listdir(downloadsPath + namePlaylistsDir)))*100) + "%")
+    #         print("Converting " + i)
+    #         playlist = CSV_Extraction(downloadsPath + namePlaylistsDir, pathToMusic, i, v)
+    #         counterAdder = playlistFileCreation(pathToFinalPlaylist, playlist)
+    #         if counterAdder > 0:
+    #             filesMissingPerPlaylist.append((str(counterAdder) + "\tsongs missing from:\t" + i))
+    #         counter += counterAdder
+    #         os.system('cls' if os.name == 'nt' else 'clear')
+    #
+    # print(str(counter) + " total missing songs from library")
+    #
+    # filesMissingPerPlaylist.sort()
+    #
+    # try:
+    #     for i in filesMissingPerPlaylist:
+    #         print(i)
+    #         logging.info(i)
+    #
+    # except:
+    #     print(filesMissingPerPlaylist)
+    #     logging.info(filesMissingPerPlaylist)
