b4f5a3b6007530c4739af8579110e843ac0a1c8b
Ryan Urquhart <ryan1328@hotmail.com>
2024-09-02 09:41:32 +0100

    WIP

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
     
     os.system('cls' if os.name == 'nt' else 'clear')
     
+    print("Starting playlist app...")
+    
     # x = threading.Thread(target=downloadPlaylists)
     # x.daemon = args.d
     # try:
