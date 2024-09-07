ba5795479619e0f95ceda6cec39e5868b039395e
Ryan Urquhart <ryan1328@hotmail.com>
2024-09-07 22:30:31 +0100

    Starting to get tests together

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,3 +3,15 @@ import settings
 def testInit():
     settings.init()
     assert settings.cachePath is not None
+    assert settings.clientId is not None
+    assert settings.clientSecret is not None
+    assert settings.convertedPlaylists is not None
+    assert settings.downloadLog is not None
+    assert settings.fileList is not None
+    assert settings.filesMissingPerPlaylist is not None
+    assert settings.globalArgs is None
+    assert settings.mainLog is not None
+    assert settings.nonLocalMusicPath is not None
+    assert settings.pathToMusic is not None
+    assert settings.pathToPlaylistDownloads is not None
+    assert settings.scope is not None
